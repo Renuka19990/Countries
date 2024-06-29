fbe730a55b71b5ebffbd4093e4398306236c97e7
renuka <renukasjagrwal@gmail.com>
2024-06-29 18:34:32 +0530

    server

--- Server/index.js
+++ Server/index.js
@@ -20,7 +20,7 @@ app.use('/api/countries', countryRoutes);
 app.use('/api/favorites', favoriteRoutes);
 app.use('/api/history', historyRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
